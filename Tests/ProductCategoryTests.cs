5b6ed3c783a842e5df468855886dce5b512d1ce2
reimootter <reimootter@gmail.com>
2017-02-20 14:23:33 +0200

    added ProductCategoryTests

---
+++ Tests/ProductCategoryTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class ProductCategoryTests
+    {
+        private ProductCategory myProductCategory;
+
+        [TestInitialize]
+        public void Init()
+        {
+            myProductCategory = new ProductCategory();
+        }
+
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            myProductCategory = null;
+        }
+
+        [TestMethod]
+        public void PeriodConstructorTest()
+        {
+            Assert.IsNotNull(myProductCategory);
+        }
+    }
+}
