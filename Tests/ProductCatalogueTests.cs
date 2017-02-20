ea0d55e9fa01f329bfd6d1b4f41d25b169cdafc7
Elisabethr <betiraid@hotmail.com>
2017-02-20 14:00:27 +0200

    added productcatalogue tests

--- Tests/ProductCatalogueTests.cs
+++ Tests/ProductCatalogueTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Logic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests
@@ -10,10 +11,23 @@ namespace Tests
     [TestClass]
     public class ProductCatalogueTests
     {
-        [TestMethod]
-        public void ConstructoTest()
+        private ProductCatalogue m;
+        [TestInitialize]
+        public void InitializeTest()
         {
-            
+            m = new ProductCatalogue();
+        }
+
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            m = null;
+        }
+
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            Assert.IsNotNull(m, null);
         }
     }
 }
