78cca6f84fa76aab343b7ff266f651add2552f3b
Diana <Diana@DIANA>
2017-02-20 12:28:34 +0200

    Added class ProductType

---
+++ Logic/ProductType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class ProductType
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public Period Valid { get; set; } 
+    }
+}
