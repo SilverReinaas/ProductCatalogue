ae584e162de2c1657e4fa8d050abde53f1006aa1
reimootter <reimootter@gmail.com>
2017-02-20 13:17:28 +0200

    added productcategory

---
+++ Logic/ProductCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class ProductCategory
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public Period Valid { get; set; }
+    }
+}
