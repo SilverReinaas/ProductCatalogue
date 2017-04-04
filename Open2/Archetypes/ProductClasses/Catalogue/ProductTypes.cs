20a7d786eeeb04f43be1a7c9108c9b9939188d94
mariontamm <tamm.marion@gmail.com>
2017-04-04 17:57:00 +0300

    ProductTypes

---
+++ Open2/Archetypes/ProductClasses/Catalogue/ProductTypes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Open.Aids;
+
+namespace Open.Archetypes.ProductClasses.Catalogue
+{
+    public class ProductTypes : BaseClasses.Archetypes<ProductType>
+    {
+        public static ProductTypes Instance { get; } = new ProductTypes();
+        public static ProductTypes GetProductTypes(string catalogueId)
+        {
+            var e = new ProductTypes();
+            var l = Instance.FindAll(x => x.IsThisUniqueId(catalogueId));
+            e.AddRange(l);
+            return e;
+        }
+
+        public static ProductTypes Random()
+        {
+            ProductTypes result = new ProductTypes();
+            for (int i = 0; i < GetRandom.Int16(1, 5); i++)
+            {
+                result.Add(ProductType.Random());
+            }
+            return result;
+        }
+    }
+}
