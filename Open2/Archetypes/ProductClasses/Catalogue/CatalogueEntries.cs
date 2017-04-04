3485077b85a7700e8e36359bbffced3e29b1f277
Silver Reinaas <silver.reinaas@gmail.com>
2017-04-04 19:42:27 +0300

    ads

--- Open2/Archetypes/ProductClasses/Catalogue/CatalogueEntries.cs
+++ Open2/Archetypes/ProductClasses/Catalogue/CatalogueEntries.cs
@@ -9,13 +9,25 @@ namespace Open.Archetypes.ProductClasses.Catalogue
 {
     public class CatalogueEntries : BaseClasses.Archetypes<CatalogueEntry>
     {
-        public static CatalogueEntries Instance { get; } =  new CatalogueEntries();
+        public static CatalogueEntries Instance { get; } = GenerateRandomInstance();
         public static CatalogueEntries GetCatalogueEntries(string catalogueId)
         {
             var e = new CatalogueEntries();
             var l = Instance.FindAll(x => x.CatalogueId == catalogueId);
             e.AddRange(l);
             return e;
+        }
+
+        public static CatalogueEntries GenerateRandomInstance()
+        {
+            var result = new CatalogueEntries();;
+            for (int i = 0; i < GetRandom.Int16(10, 30); i++)
+            {
+                var rand = CatalogueEntry.Random();
+                rand.CatalogueId = "abc123";
+                result.Add(rand);
+            }
+            return result;
         }
     }
 }
---
+++ Open2/Archetypes/ProductClasses/Catalogue/ProductCatalogues.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Open.Aids;
+
+namespace Open.Archetypes.ProductClasses.Catalogue
+{
+    public class ProductCatalogues : BaseClasses.Archetypes<ProductCatalogue>
+    {
+        public static ProductCatalogues Instance { get; } = new ProductCatalogues();
+
+        public static ProductCatalogues GenerateRandomInstance()
+        {
+            var result = new ProductCatalogues();;
+            for (int i = 0; i < GetRandom.Int16(1,5); i++)
+            {
+                result.Add(ProductCatalogue.Random());
+            }
+            return result;
+        }
+    }
+}
